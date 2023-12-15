b7649b3fb5bd2dfa4609fcb6cad395c9aaa0799e
Pannio <1821299140@qq.com>
2023-12-15 18:50:47 +0800

    part 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,51 @@ import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), // coerce 尝试转换为目标类型 此处为 number
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}), // coerce 尝试转换为目标类型 此处为 number
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string()
 })
 
 // 去除 id 和 date 字段 构建新类型
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
-export async function createInvoice(formData: FormData) {
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
+export async function createInvoice(prevState:State,formData: FormData) {
   // 如果表单包含比较多的字段时 可以用下面代码自动转换
   // const rawFormData = Object.fromEntries(formData.entries());
   // console.log(formData.entries(), Object.fromEntries(formData.entries()))
-  const { customerId, amount, status } = CreateInvoice.parse({
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+  
+
+  if (!validatedFields.success) {
+    // console.log(validatedFields.error.flatten().fieldErrors)
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    }
+  }
+
+  const {customerId, amount, status} = validatedFields.data
+
   // 以美分为单位存储金额 防止javascript中的浮点数精度问题 使更加准确
   const amountInCents = amount * 100;
   // 为发票创建日期创建一个格式为 "YYYY-MM-DD "的新日期
@@ -46,12 +73,20 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id:string, formData: FormData) {
-  const { customerId, amount, status } = FormSchema.parse({
+export async function updateInvoice(id:string,prevState:State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    }
+  }
+  const {customerId, amount, status} = validatedFields.data;
   const amountInCents = amount * 100;
   try{
     await sql `
