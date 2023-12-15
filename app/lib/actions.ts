8a08b881f86a823d935c2ce45ae7ee5f4c48b8f9
Pannio <1821299140@qq.com>
2023-12-15 16:01:30 +0800

    part 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,7 +17,6 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
 export async function createInvoice(formData: FormData) {
-
   // 如果表单包含比较多的字段时 可以用下面代码自动转换
   // const rawFormData = Object.fromEntries(formData.entries());
   // console.log(formData.entries(), Object.fromEntries(formData.entries()))
@@ -30,12 +29,19 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   // 为发票创建日期创建一个格式为 "YYYY-MM-DD "的新日期
   const date = new Date().toISOString().split('T')[0];
-  await sql `
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql `
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  }catch(e) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.'
+    }
+  }
   // 清空原先的缓存
   revalidatePath('/dashboard/invoices');
+  // 请注意如何在 try/catch 块之外调用重定向。这是因为重定向通过抛出错误来工作，该错误将被 catch 块捕获。为避免这种情况，您可以在 try/catch 之后调用重定向。只有当尝试成功时，才能访问重定向。
   // 重定向到发票页面
   redirect('/dashboard/invoices');
 }
@@ -47,18 +53,35 @@ export async function updateInvoice(id:string, formData: FormData) {
     status: formData.get('status'),
   })
   const amountInCents = amount * 100;
-  await sql `
-    UPDATE invoices 
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `
+  try{
+    await sql `
+      UPDATE invoices 
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `
+  }catch(e) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.'
+    }
+  }
+  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql `
-    DELETE FROM invoices WHERE id=${id}
-  `
-  revalidatePath('/dashboard/invoices');
+  throw new Error('Failed to Delete Invoice')
+  try {
+    await sql `
+      DELETE FROM invoices WHERE id=${id}
+    `
+    revalidatePath('/dashboard/invoices');
+    return {
+      message: 'Invoice Deleted.'
+    }
+  }catch(e) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.'
+    }
+  }
 }
